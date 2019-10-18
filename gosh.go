6ce90943ee3f246bb2540fd1f698b2568bd96111
hekonsek <hekonsek@gmail.com>
2019-10-18 11:09:55 +0200

    Added Cobra support.

--- gosh.go
+++ gosh.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cobra"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -9,49 +10,69 @@ import (
 	"strings"
 )
 
-func main() {
-	tmpScriptDir, err := ioutil.TempDir("", "gosh_")
-	if err != nil {
-		panic(err)
-	}
+var RootCommand = &cobra.Command{
+	Use:   "gosh script",
+	Short: "Gosh - simple Go scripting",
 
-	pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		panic(err)
-	}
-	scriptPath := filepath.Join(pwd, os.Args[1])
+	Run: func(cmd *cobra.Command, args []string) {
+		showHelpIfNeeded(cmd)
 
-	scriptBytes, err := ioutil.ReadFile(scriptPath)
-	if err != nil {
-		panic(err)
-	}
+		scriptArg := os.Args[1]
 
-	targetScriptPath := filepath.Join(tmpScriptDir, filepath.Base(os.Args[1]))
-	err = ioutil.WriteFile(targetScriptPath, scriptBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
+		tmpScriptDir, err := ioutil.TempDir("", "gosh_")
+		if err != nil {
+			panic(err)
+		}
 
-	dependencies := parseDependencies(string(scriptBytes))
+		pwd, err := filepath.Abs(filepath.Dir(os.Args[0]))
+		if err != nil {
+			panic(err)
+		}
+		scriptPath := filepath.Join(pwd, scriptArg)
 
-	targetModPath := filepath.Join(tmpScriptDir, "go.mod")
-	targetModBytes := []byte(`module gosh
+		scriptBytes, err := ioutil.ReadFile(scriptPath)
+		if err != nil {
+			panic(err)
+		}
+
+		targetScriptPath := filepath.Join(tmpScriptDir, filepath.Base(scriptArg))
+		err = ioutil.WriteFile(targetScriptPath, scriptBytes, 0644)
+		if err != nil {
+			panic(err)
+		}
+
+		dependencies := parseDependencies(string(scriptBytes))
+
+		targetModPath := filepath.Join(tmpScriptDir, "go.mod")
+		targetModBytes := []byte(`module gosh
 
 go 1.12
 
 ` + strings.Join(dependencies, "\n"))
-	err = ioutil.WriteFile(targetModPath, targetModBytes, 0644)
-	if err != nil {
-		panic(err)
-	}
+		err = ioutil.WriteFile(targetModPath, targetModBytes, 0644)
+		if err != nil {
+			panic(err)
+		}
 
-	scriptExec := exec.Command("go", "run", filepath.Base(os.Args[1]))
-	scriptExec.Dir = tmpScriptDir
-	scriptExec.Env = append(os.Environ(), "GO111MODULE=on")
-	out, err := scriptExec.CombinedOutput()
-	fmt.Print(string(out))
-	if err != nil {
-		panic(err)
+		scriptExec := exec.Command("go", "run", filepath.Base(scriptArg))
+		scriptExec.Dir = tmpScriptDir
+		scriptExec.Env = append(os.Environ(), "GO111MODULE=on")
+		out, err := scriptExec.CombinedOutput()
+		fmt.Print(string(out))
+		if err != nil {
+			panic(err)
+		}
+	},
+}
+
+func main() {
+	exitOnCliError(RootCommand.Execute())
+}
+
+func showHelpIfNeeded(cmd *cobra.Command) {
+	if len(os.Args) != 2 {
+		exitOnCliError(cmd.Help())
+		os.Exit(UnixExitCodeGeneralError)
 	}
 }
 
@@ -66,4 +87,22 @@ func parseDependencies(script string) []string {
 		}
 	}
 	return dependencies
+}
+
+// Util
+
+const UnixExitCodeGeneralError = 1
+
+func cliError(err error) bool {
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return true
+	}
+	return false
+}
+
+func exitOnCliError(err error) {
+	if cliError(err) {
+		os.Exit(UnixExitCodeGeneralError)
+	}
 }
